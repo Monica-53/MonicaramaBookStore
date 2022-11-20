d4bdfc5df9d2054af50fb85eb9f1e83fcc436f1b
Monica-53 <99419818+Monica-53@users.noreply.github.com>
2022-11-20 10:48:24 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonicaramaBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
